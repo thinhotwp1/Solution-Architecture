677ae73a6946e2e1050086a847976c71d69d94ea
thinh <leduythinh20001@gmail.com>
2026-04-15 00:15:56 +0700

    init

--- README.md
+++
@@ -1,4 +0,0 @@
-# Day 6: VPC Foundation & CIDR Blocks	Dùng Terraform định nghĩa 1 VPC với dải IP 10.0.0.0/16
-
-Follow this guild:
-https://docs.google.com/document/d/19qRMuUgp80Ew9xTKGqDvym2E7EfxQ70rk8_9kM_q4ws/edit?usp=sharing
--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
+# --- Provider Configuration (Giữ lại để kết nối LocalStack) ---
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.65" # Hạ version xuống 4.x để tương thích hoàn hảo với LocalStack Free
+      version = "~> 4.65"
     }
   }
 }
@@ -15,14 +16,14 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
-  # Trỏ tất cả yêu cầu IAM về LocalStack (nằm ở port 4566)
   endpoints {
     iam = "http://localstack:4566"
     sts = "http://localstack:4566"
+    ec2 = "http://localstack:4566"
   }
 }
 
-# Day 6: Khởi tạo một VPC với dải IP 10.0.x.x
+# --- Day 7: VPC Declaration ---
 resource "aws_vpc" "main_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -32,33 +33,37 @@ resource "aws_vpc" "main_vpc" {
     Name = "Aviation-Core-VPC"
   }
 }
-# Tạo Public Subnet (Dành cho Load Balancer / API Gateway)
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.1.0/24" # Cắt ra 256 IPs
+
+# --- Subnets Allocation ---
+
+# AZ 1a
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id                  = aws_vpc.main_vpc.id # Tham chiếu đúng tên "main_vpc" ở trên
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true # Tự động cấp IP Public cho máy ảo gắn vào đây
-
-  tags = {
-    Name = "Public-Subnet-1"
-  }
+  map_public_ip_on_launch = true
+  tags = { Name = "Public-Subnet-1a" }
 }
 
-# Tạo Private Subnet (Dành cho Java Backend / Database)
-resource "aws_subnet" "private_subnet_1" {
+resource "aws_subnet" "private_subnet_1a" {
   vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.2.0/24" # Cắt ra thêm 256 IPs
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "Private-Subnet-1"
-  }
+  tags = { Name = "Private-Subnet-1a" }
 }
-# Create gateway to take traffic
-resource "aws_internet_gateway" "main_igw" {
-  vpc_id = aws_vpc.main_vpc.id
 
-  tags = {
-    Name = "Main-Internet-Gateway"
-  }
+# AZ 1b
+resource "aws_subnet" "public_subnet_1b" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = { Name = "Public-Subnet-1b" }
+}
+
+resource "aws_subnet" "private_subnet_1b" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+  tags = { Name = "Private-Subnet-1b" }
 }
