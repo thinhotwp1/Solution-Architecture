fd4a2ac208488c1f4359e8b5332b7894595f6d5c
thinhle_ctr <thinhle_ctr@fortna.com>
2026-04-14 18:03:21 +0700

    init

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.65" # Hạ version xuống 4.x để tương thích hoàn hảo với LocalStack Free
+    }
+  }
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  # Trỏ tất cả yêu cầu IAM về LocalStack (nằm ở port 4566)
+  endpoints {
+    iam = "http://localstack:4566"
+    sts = "http://localstack:4566"
+  }
+}
+
 # Day 6: Khởi tạo một VPC với dải IP 10.0.x.x
 resource "aws_vpc" "main_vpc" {
   cidr_block           = "10.0.0.0/16"
