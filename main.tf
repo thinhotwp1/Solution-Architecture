bd1962edb888995d16acf1e425ed3364e11502b5
thinh <leduythinh20001@gmail.com>
2026-04-17 01:02:27 +0700

    init

--- main.tf
+++ main.tf
@@ -34,6 +34,39 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
+# --- Subnets ---
+# AZ 1a
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags                    = { Name = "Public-Subnet-1a" }
+}
+
+resource "aws_subnet" "private_subnet_1a" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+  tags              = { Name = "Private-Subnet-1a" }
+}
+
+# AZ 1b
+resource "aws_subnet" "public_subnet_1b" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags                    = { Name = "Public-Subnet-1b" }
+}
+
+resource "aws_subnet" "private_subnet_1b" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+  tags              = { Name = "Private-Subnet-1b" }
+}
+
 # Day 11: 1. Create Web Security Group (For Load Balancer or Web EC2)
 resource "aws_security_group" "web_sg" {
   name        = "Aviation-Web-SG"
@@ -93,4 +126,51 @@ resource "aws_security_group" "db_sg" {
   }
 
   tags = { Name = "DB-Security-Group" }
+}
+
+# Day 12: 1. Create a Custom NACL for the Public Subnet
+resource "aws_network_acl" "public_nacl" {
+  vpc_id     = aws_vpc.main_vpc.id
+  subnet_ids = [aws_subnet.public_subnet_1a.id] # Attach to Subnet 1a
+
+  tags = { Name = "Aviation-Public-NACL" }
+}
+
+# --- INBOUND RULES (Luồng đi vào) ---
+
+# Rule 100: Deny Hacker IP (Evaluated FIRST because number is lowest)
+resource "aws_network_acl_rule" "deny_hacker_inbound" {
+  network_acl_id = aws_network_acl.public_nacl.id
+  rule_number    = 100
+  egress         = false # false means Inbound
+  protocol       = "-1"  # All protocols
+  rule_action    = "deny"
+  cidr_block     = "203.0.113.50/32" # Example malicious IP
+}
+
+# Rule 200: Allow HTTP from anywhere
+resource "aws_network_acl_rule" "allow_http_inbound" {
+  network_acl_id = aws_network_acl.public_nacl.id
+  rule_number    = 200
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 80
+  to_port        = 80
+}
+
+# --- OUTBOUND RULES (Luồng đi ra) ---
+
+# Rule 100: Allow HTTP response out via Ephemeral Ports
+# (Cho phép trả kết quả HTTP về qua các cổng tạm thời)
+resource "aws_network_acl_rule" "allow_ephemeral_outbound" {
+  network_acl_id = aws_network_acl.public_nacl.id
+  rule_number    = 100
+  egress         = true # true means Outbound
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 1024
+  to_port        = 65535 # Ephemeral port range
 }
