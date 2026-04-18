658d74944fbdf250bb0e8ec195ad1cbdeeee7029
thinh <leduythinh20001@gmail.com>
2026-04-18 19:42:38 +0700

    init

--- main.tf
+++ main.tf
@@ -410,6 +410,42 @@ resource "aws_network_acl_rule" "allow_ephemeral_outbound" {
   to_port        = 65535 # Ephemeral port range
 }
 
+# Day 13:  Tạo VPC thứ 2 cho hệ thống
+resource "aws_vpc" "shared_vpc" {
+  cidr_block           = "10.1.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = { Name = "Aviation-Shared-VPC" }
+}
+
+# Tạo một Route Table mặc định cho Shared VPC
+resource "aws_route_table" "shared_rt" {
+  vpc_id = aws_vpc.shared_vpc.id
+  tags   = { Name = "Shared-Route-Table" }
+}
+
+# Khởi tạo cầu nối Peering giữa Core VPC và Shared VPC
+resource "aws_vpc_peering_connection" "core_to_shared" {
+  vpc_id        = aws_vpc.main_vpc.id   # Người yêu cầu (Requester) - Core VPC
+  peer_vpc_id   = aws_vpc.shared_vpc.id # Người nhận (Accepter) - Shared VPC
+  auto_accept   = true                  # Tự động chấp thuận
+
+  tags = { Name = "Core-to-Shared-Peering" }
+}
+# 1. Chỉ đường cho Core VPC: "Muốn sang mạng 10.1.x.x thì đi qua cầu Peering"
+resource "aws_route" "core_to_shared_route" {
+  route_table_id            = aws_route_table.private_rt.id # Trỏ từ Private Route Table A của Core VPC
+  destination_cidr_block    = aws_vpc.shared_vpc.cidr_block   # Đích đến: 10.1.0.0/16
+  vpc_peering_connection_id = aws_vpc_peering_connection.core_to_shared.id
+}
+
+# 2. Chỉ đường cho Shared VPC: "Muốn gọi ngược lại mạng 10.0.x.x thì cũng đi qua cầu Peering"
+resource "aws_route" "shared_to_core_route" {
+  route_table_id            = aws_route_table.shared_rt.id  # Route Table của Shared VPC
+  destination_cidr_block    = aws_vpc.main_vpc.cidr_block   # Đích đến: 10.0.0.0/16
+  vpc_peering_connection_id = aws_vpc_peering_connection.core_to_shared.id
+}
+
 # Day 17: 1. Fetch the latest Amazon Linux 2023 AMI dynamically
 data "aws_ami" "amazon_linux_2023" {
   most_recent = true
