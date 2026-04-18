448af2d92c8f35993236d6137a418013100dcf42
thinh <leduythinh20001@gmail.com>
2026-04-18 19:35:50 +0700

    init

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_iam_group" "developers" {
   name = "Developers"
 }
 
-# Day 1: Tạo 2 IAM Users (Người dùng)
+# Tạo 2 IAM Users (Người dùng)
 resource "aws_iam_user" "senior_dev" {
   name = "Thinh-Senior"
 }
@@ -37,7 +37,7 @@ resource "aws_iam_user" "junior_dev" {
   name = "Junior-Dev"
 }
 
-# Day 1: Thiết lập Membership (Thành viên) để gán 2 user vào group
+# Setup Membership and assign 2 member into group
 resource "aws_iam_group_membership" "dev_team" {
   name = "dev-membership"
   users = [
@@ -46,6 +46,36 @@ resource "aws_iam_group_membership" "dev_team" {
   ]
   group = aws_iam_group.developers.name
 }
+
+# Day 2: Create a custom IAM Policy for S3 ReadOnly access
+resource "aws_iam_policy" "s3_readonly_policy" {
+  name        = "S3-ReadOnly-Custom"
+  description = "Provides read-only access to all S3 buckets"
+
+  # Using 'jsonencode' is a best practice to generate JSON safely in Terraform
+  policy = jsonencode({
+    Version = "2012-10-17" # This is the standard policy version date required by AWS
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:Get*", # Allows downloading objects
+          "s3:List*" # Allows viewing buckets
+        ]
+        Resource = "*" # Applies to all S3 resources
+      }
+    ]
+  })
+}
+
+# Attach the custom policy to the "Developers" group created in Day 1
+resource "aws_iam_group_policy_attachment" "dev_s3_readonly_attach" {
+  group = aws_iam_group.developers.name
+
+  # We reference the ARN (Amazon Resource Name) of the policy we just created
+  policy_arn = aws_iam_policy.s3_readonly_policy.arn
+}
+
 
 
 # --- Day 7: VPC Declaration ---
