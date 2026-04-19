3ff77ceb560c1ae2b929e6dc730c4e5410d9175d
thinh <leduythinh20001@gmail.com>
2026-04-20 01:28:29 +0700

    init

--- main.tf
+++ main.tf
@@ -26,58 +26,32 @@ provider "aws" {
   }
 }
 
-# Day 22: 1. Create a Customer Managed Key (KMS Key)
-resource "aws_kms_key" "sia_s3_key" {
-  description             = "KMS key to encrypt SIA passenger documents"
-  deletion_window_in_days = 7
-  enable_key_rotation     = true # Tự động luân chuyển khóa mỗi năm (Best Practice)
-
-  tags = { Name = "SIA-KMS-S3-Key" }
-}
-
-# Tạo một Alias (Tên gợi nhớ) cho KMS Key để dễ sử dụng
-resource "aws_kms_alias" "sia_s3_key_alias" {
-  name          = "alias/sia-passenger-docs-key"
-  target_key_id = aws_kms_key.sia_s3_key.key_id
-}
-
-# 2. Create the Secure S3 Bucket
-resource "aws_s3_bucket" "sia_secure_docs" {
+# Day 23: 1. Create the  S3 Bucket
+resource "aws_s3_bucket" "sia_documents_bucket" {
   bucket = "sia-secure-passenger-data-ldt-2026"
   tags   = { Name = "SIA-Secure-Docs" }
 }
 
-# 3. Enable Versioning
-resource "aws_s3_bucket_versioning" "sia_secure_versioning" {
-  bucket = aws_s3_bucket.sia_secure_docs.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+resource "aws_s3_bucket_lifecycle_configuration" "sia_archive_policy" {
+  bucket = aws_s3_bucket.sia_documents_bucket.id
 
-# 4. Enforce Server-Side Encryption with our KMS Key
-resource "aws_s3_bucket_server_side_encryption_configuration" "sia_secure_encryption" {
-  bucket = aws_s3_bucket.sia_secure_docs.id
-
+  # Rule 1: Chuyển dữ liệu cũ sang Glacier
   rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.sia_s3_key.arn
-      sse_algorithm     = "aws:kms"
+    id     = "Archive-To-Glacier-After-30-Days"
+    status = "Enabled"
+
+    # Lọc: Áp dụng rule này cho toàn bộ bucket (bạn có thể thêm filter_prefix nếu chỉ muốn áp dụng cho 1 thư mục cụ thể)
+    filter {}
+
+    # Hành động: Chuyển đổi hạng lưu trữ
+    transition {
+      days          = 30
+      storage_class = "GLACIER"
     }
-    # Ép buộc mã hóa ngay cả khi user quên gửi header mã hóa lúc upload
-    bucket_key_enabled = true
-  }
-}
 
-# 5. Configure CORS (Allow a React frontend on a specific domain to upload)
-resource "aws_s3_bucket_cors_configuration" "sia_secure_cors" {
-  bucket = aws_s3_bucket.sia_secure_docs.id
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST", "GET"]
-    allowed_origins = ["https://www.singaporeair.com", "http://localhost:3000"] # Chặn mọi domain khác
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
+    # Thực hành tốt (Best Practice): Dọn dẹp luôn các bản upload bị lỗi/treo
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
   }
 }
